64b8c675d468d283e290c0224cd4528648d3d757
knuxus <knuxus@gmail.com>
2014-03-05 10:05:47 +0000

    falta so o ajax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,4 +4,27 @@ function recentWork(){
     $('#quote').hide('slow');
     $('#friends').hide('slow');
     $('#friends-logos').hide('slow');
+    $('#projects').show('slow');
+    $('#work').show('slow');
+
+}
+
+function resume(){
+	"use strict";
+	$('#quote').show('slow');
+	$('#main').hide('slow');
+	$('#projects').hide('slow');
+    $('#work').hide('slow');
+	$('#friends').hide('slow');
+    $('#friends-logos').hide('slow');
+}
+
+function contact(){
+	"use strict";
+	$('#quote').show('slow');
+	$('#main').hide('slow');
+	$('#projects').hide('slow');
+    $('#work').hide('slow');
+	$('#friends').hide('slow');
+    $('#friends-logos').hide('slow');
 }
